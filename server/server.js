6c2887b95011ad24f8b9fa8f7213864be6e6d051
Alexandre Bolzon <blzn@mail.ru>
2020-10-14 22:51:14 -0300

    Inserting reandom time in server login response

--- server/server.js
+++ server/server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const MIN_LOGIN_RESPONSE = 150;
+const MAX_LOGIN_RESPONSE = 700;
+
+const getRandomTime = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 app.get('/', (req, res) => {
   res.send('It is my home page!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.get('/login', (req, res) => {
+  const responseTime = getRandomTime(MIN_LOGIN_RESPONSE, MAX_LOGIN_RESPONSE);
+  const responseMsg = `It is my login page! Took ${responseTime}ms to send response.`;
+  setTimeout(() => res.send(responseMsg), responseTime);
 });
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
