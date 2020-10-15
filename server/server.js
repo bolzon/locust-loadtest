87f39f3c0d53f27956603ece756df936b5d9b9ca
Alexandre Bolzon <blzn@mail.ru>
2020-10-14 23:43:49 -0300

    Adding products page in server

--- server/server.js
+++ server/server.js
@@ -2,18 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// time taken in ms to login page send a response
 const MIN_LOGIN_RESPONSE = 150;
-const MAX_LOGIN_RESPONSE = 700;
+const MAX_LOGIN_RESPONSE = 500;
+
+// time taken in ms to products page send a response
+const MIN_PRODUCTS_RESPONSE = 400;
+const MAX_PRODUCTS_RESPONSE = 1300;
+
+///////////////////////////////////////
 
 const getRandomTime = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 app.get('/', (req, res) => {
-  res.send('It is my home page!');
+  res.send('It is the home page!');
 });
 
 app.get('/login', (req, res) => {
   const responseTime = getRandomTime(MIN_LOGIN_RESPONSE, MAX_LOGIN_RESPONSE);
-  const responseMsg = `It is my login page! Took ${responseTime}ms to send response.`;
+  const responseMsg = `It is the login page! Took ${responseTime}ms to send response.`;
+  setTimeout(() => res.send(responseMsg), responseTime);
+});
+
+app.get('/products', (req, res) => {
+  const responseTime = getRandomTime(MIN_PRODUCTS_RESPONSE, MAX_PRODUCTS_RESPONSE);
+  const responseMsg = `It is the products page! Took ${responseTime}ms to send response.`;
   setTimeout(() => res.send(responseMsg), responseTime);
 });
 
